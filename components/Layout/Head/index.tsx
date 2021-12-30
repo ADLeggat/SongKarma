97f21a6d39a421ec48a234180eec775b04638b9d
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2021-12-30 08:59:22 +0000

    SK-1 create Head component

---
+++ components/Layout/Head/index.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import NextHead from "next/head";
+
+interface Props {
+    title: string;
+};
+
+const Head: FC<Props> = ({ title }) => {
+    return (
+        <NextHead>
+            <title> {title} </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+            <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet"/>
+            <link rel="shortcut icon" href=""/>
+        </NextHead>
+    );
+};
+
+export default Head;
