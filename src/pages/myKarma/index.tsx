7c58cdf9bbb3e77482687d02b6d33222a5b19be4
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2021-12-31 09:32:07 +0000

    SK-3 add my karma page stub

---
+++ src/pages/myKarma/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "../../components/Layout";
+const index = () => {
+    return (
+        <Layout path="myKarma" title="My Karma">
+            MY KARMA
+        </Layout>
+    );
+}
+
+export default index;
