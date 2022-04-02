e268f319217661ee1fc072e7bb8a0f983e836b5c
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-04-02 12:16:07 +0100

    SK-11 add strapline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import { NextPage } from "next";
 import Layout from "~/components/Layout";
+import styles from "./index.module.scss";
 
 const Home: NextPage = () => {
     return (
         <div>
             <Layout path="/" title="Home">
-                SIGN UP PAGE
+                <div className={styles.Strapline}>
+                    The Peer <br/>
+                    To Peer <br/>
+                    Songwriter <br/>
+                    Charts
+                </div>
             </Layout>
         </div>
     );
