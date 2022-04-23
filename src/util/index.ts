45e3290b2e726f2e032153f995a06ec8cea1df63
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-04-23 13:12:45 +0100

    SK-12 add try ctach method to util index

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,2 +1,13 @@
 export * from "./constants";
 export * from "./formValidation";
+
+export type VoidPromiseFunction = (...params: unknown[]) => Promise<void>;
+export type OnError = (err: Error) => void;
+
+export const attempt = async (func: VoidPromiseFunction, onError: OnError) => {
+    try {
+        await func();
+    } catch(err) {
+        onError(err as Error);
+    }
+};
