3bdd5c67a41186ae5a7513540eeb92fcfdaaac64
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-04-23 13:30:32 +0100

    SK-12 export utils from index

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,8 @@
+export * from "./api";
+export * from "./auth";
 export * from "./constants";
 export * from "./formValidation";
+export * from "./user";
 
 export type VoidPromiseFunction = (...params: unknown[]) => Promise<void>;
 export type OnError = (err: Error) => void;
