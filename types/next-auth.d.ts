8f5f523be616632efd26ea76a31343af020acd47
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-04-23 13:13:00 +0100

    SK-12 add types to next-auth

---
+++ types/next-auth.d.ts
@@ -0,0 +1,13 @@
+import { Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
+// import { ObjectId } from "mongodb";
+
+declare module "next-auth" {
+    interface Session {
+        user: User;
+    }
+
+    interface User {
+        username: string;
+    }
+}
