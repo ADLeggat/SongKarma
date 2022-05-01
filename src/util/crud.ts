a9f143bdad585bc0ec466bcd77129d181c6cb0e8
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-05-01 11:51:10 +0100

    SK-12 create crud util

---
+++ src/util/crud.ts
@@ -0,0 +1,25 @@
+import { NextApiResponse } from "next";
+import { ApiRequest, tryCatchAsync } from "~/util";
+import { Crud } from "./constants";
+import { validate } from "./formValidation";
+
+
+export const createWithValidation = async (req: ApiRequest, res: NextApiResponse, tableName: string, func: Function) => {
+    const errors = await validate(req);
+    if(errors.length !== 0){
+        return getValidationErrorMessage(tableName, errors);
+    }
+
+    tryCatchAsync(
+        async () => await func(), 
+        err => getCrudErrorMessage(tableName, Crud.CREATING)
+    );
+};
+
+const getValidationErrorMessage = (tableName: string, errors: string[]) => {
+    return `${tableName} validation errors: ${errors}`;
+}
+
+const getCrudErrorMessage = (tableName: string, operation: string) => {
+    return `There was a problem ${operation} the ${tableName}`;
+};
