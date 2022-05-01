c51d5d545f73c32d6c1e9eecf9c134618cba8c12
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-05-01 11:37:54 +0100

    SK-12 create validate method

--- src/util/formValidation.ts
+++ src/util/formValidation.ts
@@ -1,5 +1,15 @@
+import { NextApiRequest } from "next";
 import * as yup from 'yup';
-import { UserEntity } from "./constants";
+import { ApiRequest, tryCatchAsync, UserEntity } from "~/util";
+
+export const validate = async (req: ApiRequest) => {
+    const { validations, body } = req;
+    tryCatchAsync(
+        () => validations.validate(body, { abortEarly: false }), 
+        err => (err as yup.ValidationError).errors
+    );
+    return [];
+};
 
 const isRequired = (table: string, field: string) => {
     const formattedTable = table === "user"? "": `${table}'s`;
