3e6dcf75721f213a58ef24356a12aeab8ed0a019
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-05-01 11:38:48 +0100

    SK-12 create ApiRequest interface

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,3 +1,6 @@
+import { NextApiRequest } from "next";
+import { ObjectSchema } from "yup";
+
 const HEADERS = {
     "Accept": "application/json",
     "Content-Type": "application/json"
@@ -8,6 +11,10 @@ export const POST = "POST";
 export const PUT = "PUT";
 export const PATCH = "PATCH";
 export const DELETE = "DELETE";
+
+export interface ApiRequest extends NextApiRequest {
+    validations: ObjectSchema<any, any, any, any>
+};
 
 export const doCallout = async (method: string, endpoint: string, body?: unknown) => {
     const params: RequestInit = {
