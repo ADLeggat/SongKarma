f450b8508f8fcfa39295d0279ffc40ee2c2f5de0
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-05-01 12:23:18 +0100

    SK-12 create signup method

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,3 +1,10 @@
+import { NextApiResponse } from "next";
+import { User } from "@prisma/client";
+import db from "../../prisma/prisma";
+import { 
+    ApiRequest, buildResponse, createWithValidation, generateToken, passwordValidation, userDetailsValidation, UserEntity 
+} from "~/util";
+
 interface LoginResponse {
     user: SessionUser
 }
@@ -5,8 +12,41 @@ interface LoginResponse {
 interface SessionUser {
     id: string;
     username: string;
-}
+};
+
+export const signup = async (req: ApiRequest, res: NextApiResponse) => {
+    req.validations = userDetailsValidation.concat(passwordValidation);
+
+    return createWithValidation(req, res, UserEntity.TABLE_NAME, async () => {
+        let user = await db.user.findUnique({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        if(!user) {
+            const authToken = await generateToken(32);
+            user = await db.user.create({
+                data: {
+                    ...req.body,
+                    authToken
+                }
+            });
+            const sessionUser = createSessionUser(user);
+            return buildResponse(true, sessionUser);
+        } else {
+            return buildResponse(false, null);
+        }
+    });
+};
+
+const createSessionUser = (user: User) => {
+    return {
+        id: user.id,
+        username: user.username
+    };
+};
 
 export const login = async (credentials: Record<string, string>): Promise<LoginResponse> => {
     return { user: { id: "1", username: "test" } };
-}
+};
