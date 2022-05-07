575ef3258735fc6ef49375ec75105f16e042a03e
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-05-07 14:07:18 +0100

    SK-12 use Routes class in NavBar

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Nav from "react-bootstrap/Nav";
 import NavLink from "~/components/Layout/NavLink";
 import styles from "./index.module.scss";
-import { protectedRoutes } from "~/util";
+import { Routes } from "~/util";
 import { Session } from "next-auth";
 
 interface Props {
@@ -14,6 +14,10 @@ interface Props {
 const Navbar = (props: Props) => {
     const { path, session } = props;
 
+    const pathMatches = (match: string) => {
+        return path === match;
+    }
+
     return (
         <BsNavbar expand="lg" className={styles.NavStyle}>
             <BsNavbar.Brand href='/' className={styles.Brand}>
@@ -22,20 +26,20 @@ const Navbar = (props: Props) => {
             <BsNavbar.Toggle aria-controls="responsive-navbar-nav" />
             <BsNavbar.Collapse id="responsive-navbar-nav">
                 <Nav>
-                    <NavLink href="/howItWorks" link="How it Works" isActive={path === "/howItWorks"} />
+                    <NavLink href={Routes.HOW_IT_WORKS} link="How it Works" isActive={pathMatches(Routes.HOW_IT_WORKS)} />
                     
                     {session &&
                         <>
-                            <NavLink href="/news" link="Karma News" isActive={path === "/news"} />
-                            <NavLink href="/myKarma" link="My Karma" isActive={path === "/myKarma"} />
-                            <NavLink href="/charts" link="Charts" isActive={path === "/charts"} />
-                            <NavLink href="/radio" link="Radio" isActive={path === "/radio"} />
-                            <NavLink href="/contact" link="Contact" isActive={path === "/contact"} />
+                            <NavLink href={Routes.NEWS} link="Karma News" isActive={pathMatches(Routes.NEWS)} />
+                            <NavLink href={Routes.MY_KARMA} link="My Karma" isActive={pathMatches(Routes.MY_KARMA)} />
+                            <NavLink href={Routes.CHARTS} link="Charts" isActive={pathMatches(Routes.CHARTS)} />
+                            <NavLink href={Routes.RADIO} link="Radio" isActive={pathMatches(Routes.RADIO)} />
+                            <NavLink href={Routes.CONTACT} link="Contact" isActive={pathMatches(Routes.CONTACT)} />
                         </>
                     }
                     {!session && 
                         <>
-                            <NavLink href="/auth" link="Login/Register" isActive={path === "/auth"} />
+                            <NavLink href={Routes.AUTH} link="Login/Register" isActive={pathMatches(Routes.AUTH)} />
                         </>
                     }
                 </Nav>
