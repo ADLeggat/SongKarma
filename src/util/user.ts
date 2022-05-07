f2f8c0072cfadda4180cad5c0dbd8135ad63283a
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-05-07 13:52:38 +0100

    SK-12 create UserLoginFormFields interface

--- src/util/user.ts
+++ src/util/user.ts
@@ -1,14 +1,17 @@
 import { doCallout, POST } from "~/util/api";
 
-export interface UserDetailsFormFields {
-    email: string;
-    password: string;
+export interface UserDetailsFormFields extends UserLoginFormFields {
     confirmPassword: string;
     username: string;
     artistName: string,
     artistBio?: string;
     profilePic?: string;
     receiveNews: boolean;
+};
+
+export interface UserLoginFormFields {
+    email: string;
+    password: string;
 };
 
 export const signup = async (fields: unknown) => {
