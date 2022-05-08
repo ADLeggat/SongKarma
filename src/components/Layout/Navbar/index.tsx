fbcecd1fe8806e7f9041fba9b6f901e917086872
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-05-08 13:37:30 +0100

    SK-12 add nav dropdown with logout

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,6 +1,6 @@
-import BsNavbar from "react-bootstrap/Navbar";
 import Image from "next/image";
-import Nav from "react-bootstrap/Nav";
+import { signOut } from "next-auth/react";
+import { Nav, Navbar as BsNavbar, NavDropdown } from "react-bootstrap";
 import NavLink from "~/components/Layout/NavLink";
 import styles from "./index.module.scss";
 import { Routes } from "~/util";
@@ -16,11 +16,15 @@ const Navbar = (props: Props) => {
 
     const pathMatches = (match: string) => {
         return path === match;
-    }
+    };
+
+    const logout = () => {
+        signOut();
+    };
 
     return (
         <BsNavbar expand="lg" className={styles.NavStyle}>
-            <BsNavbar.Brand href='/' className={styles.Brand}>
+            <BsNavbar.Brand href={session ? Routes.MY_KARMA : '/'} className={styles.Brand}>
                 <Image src="/brand.png" width={260} height={200}/>
             </BsNavbar.Brand>
             <BsNavbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -35,6 +39,19 @@ const Navbar = (props: Props) => {
                             <NavLink href={Routes.CHARTS} link="Charts" isActive={pathMatches(Routes.CHARTS)} />
                             <NavLink href={Routes.RADIO} link="Radio" isActive={pathMatches(Routes.RADIO)} />
                             <NavLink href={Routes.CONTACT} link="Contact" isActive={pathMatches(Routes.CONTACT)} />
+
+                            <ul className={`${styles.Dropdown}`}>
+                                <NavDropdown title={`LOGGED IN AS ${session?.user.username}`}>
+                                    <NavDropdown.Item>
+                                        Settings
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#" onClick={logout}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            </ul >
+                                
                         </>
                     }
                     {!session && 
