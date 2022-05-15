64778ec80fd06b6dc4debcfe98932ab7d9db4417
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-05-15 12:02:38 +0100

    SK-12 use SSRProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import { SessionProvider } from 'next-auth/react';
 import "~/styles/globals.scss";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { SSRProvider } from "react-bootstrap";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-		<SessionProvider session={pageProps.session}>
-			<Component {...pageProps} />
-		</SessionProvider>
+		<SSRProvider>
+			<SessionProvider session={pageProps.session}>
+				<Component {...pageProps} />
+			</SessionProvider>
+		</SSRProvider>
 	);
 }
 
