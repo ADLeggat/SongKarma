167eaae69e0c07aa231e0f56bd91bbd12eb3a56b
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-05-16 11:24:50 +0100

    SK-12 add new styles to modal

--- src/components/UI/Modal/index.tsx
+++ src/components/UI/Modal/index.tsx
@@ -1,5 +1,6 @@
 import { Modal } from "react-bootstrap";
 import { FaTimes } from "react-icons/fa";
+import styles from "./index.module.scss";
 
 interface Props {
     children: React.ReactNode;
@@ -12,10 +13,10 @@ function index(props: Props) {
     const { children, onHide, showModal, title } = props;
     return (
         <Modal show={showModal} onHide={onHide}>
-            <Modal.Body>
+            <Modal.Body className={styles.ModalBody}>
                  <h5 className="modal-title text_header mb-3 d-flex justify-content-between">
                      {title}
-                     <span className="text_header cross" onClick={onHide}>
+                     <span className={styles.Cross} onClick={onHide}>
                          <FaTimes/>
                      </span>
                  </h5>
