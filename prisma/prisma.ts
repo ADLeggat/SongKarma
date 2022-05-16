564f098796506fff5df935b7a69d67ad10336ee1
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-05-16 06:16:37 +0100

    SK-12 create prisma mock for testing

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,14 +1,30 @@
 import { PrismaClient } from "@prisma/client";
+import { getPrismaMock } from "~/util";
 
 declare global {
     var prisma: PrismaClient
 }
 
+const createPrismaClient = () => {
+    const prisma = new PrismaClient();
+
+    if(process.env.NODE_ENV === "test") {
+        return getPrismaMock(prisma);
+    }
+
+    return prisma;
+};
+
 if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
+    prisma = createPrismaClient();
 } else {
     if (!global.prisma) {
-        global.prisma = new PrismaClient();
+        global.prisma = createPrismaClient();
+        // if(process.env.NODE_ENV === "test") {
+        //     global.prisma = prismaMock;
+        // } else {
+        //     global.prisma = new PrismaClient();
+        // }
     }
     prisma = global.prisma;
 }
