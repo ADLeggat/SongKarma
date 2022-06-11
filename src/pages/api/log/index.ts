e8f984b3f69374b699bfcfe93cd2cabd4cfb0f81
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-06-11 11:47:33 +0100

    SK-16 return response from log api route

--- src/pages/api/log/index.ts
+++ src/pages/api/log/index.ts
@@ -1,16 +1,13 @@
 import { NextApiResponse } from "next";
 import { log } from "~/controllers";
-import { Api, ApiRequest, createJsonPayload, POST } from "~/util";
+import { Api, ApiRequest, createJsonPayload, LogData, POST } from "~/util";
 
 
 async function handler(req: ApiRequest, res: NextApiResponse) {
     switch(req.method) {
         case POST:
-            await log({
-                type: req.body.type,
-                context: req.body.context,
-                message: req.body.message
-            });
+            await log(req.body);
+            return res.send(createJsonPayload(false, Api.SOMETHING_WENT_WRONG));
             break;
         default:
             const notAllowedRes = createJsonPayload(false, Api.METHOD_NOT_ALLOWED);
