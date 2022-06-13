9b08ea54bb9391510aaab7018eae57af3e8a71ff
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-06-13 09:35:12 +0100

    SK-16 add logging to createWithValidation

--- src/util/crud.ts
+++ src/util/crud.ts
@@ -1,10 +1,13 @@
 import { NextApiResponse } from "next";
-import { ApiRequest, createJsonPayload } from "~/util";
+import { getSession } from "next-auth/react";
+import { log } from "~/controllers";
+import { ApiRequest, createJsonPayload, LogContexts, LogTypes } from "~/util";
 import { Crud } from "./constants";
 import { validate } from "./formValidation";
 
 
 export const createWithValidation = async (req: ApiRequest, res: NextApiResponse, tableName: string, create: Function) => {
+    const session = await getSession();
     const errors = await validate(req);
     if(errors.length !== 0){
         return getValidationErrorMessage(tableName, errors);
@@ -13,6 +16,12 @@ export const createWithValidation = async (req: ApiRequest, res: NextApiResponse
     try {
         return await create();
     } catch(err) {
+        await log({
+            userId: session!.user.id,
+            type: LogTypes.ERROR,
+            context: LogContexts.CRUD,
+            message: (err as Error).message
+        });
         return createJsonPayload(false, getCrudErrorMessage(tableName, Crud.CREATING));
     }
 };
