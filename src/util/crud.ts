2cbfd7eca562d73ef67121b9972f9e4d14b86bc7
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-06-16 14:49:09 +0100

    SK-17 create getRecords method

--- src/util/crud.ts
+++ src/util/crud.ts
@@ -1,10 +1,24 @@
-import { NextApiResponse } from "next";
+import { Session } from "next-auth";
 import { getSession } from "next-auth/react";
-import { log } from "~/controllers";
 import { ApiRequest, createJsonPayload, doCallout, LogContexts, LOGGING_URI, LogTypes, POST } from "~/util";
 import { Crud } from "./constants";
 import { validate } from "./formValidation";
 
+export const getRecords = async (tableName: string, func: Function) => {
+    let session = await getSession();
+    try{
+        const records = await func();
+        return createJsonPayload(true, getCrudErrorMessage(tableName, Crud.RETRIEVED), records);
+    } catch(err){
+        await doCallout(POST, `${process.env.NEXTAUTH_URL}/${LOGGING_URI}`, {
+            userId: session?.user.id,
+            type: LogTypes.ERROR,
+            context: LogContexts.CRUD,
+            message: (err as Error).message
+        });
+        return createJsonPayload(false, getCrudErrorMessage(tableName, Crud.RETRIEVING));
+    }
+};
 
 export const createWithValidation = async (req: ApiRequest, tableName: string, create: Function) => {
     const session = await getSession();
