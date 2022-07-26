33666d2341fd1958c28318df2d1ea46214db2019
ADLeggat <34620369+ADLeggat@users.noreply.github.com>
2022-07-26 14:05:46 +0100

    remove console log

--- src/components/Auth/SignUp/index.tsx
+++ src/components/Auth/SignUp/index.tsx
@@ -38,7 +38,6 @@ const index = (props: Props) => {
 
     const doSignUp = async (fields: UserDetailsFormFields) => {
         const res = await signup(fields);
-        console.log("SIGN UP RES: ", res);
 
         if(!res.success) {
             updateMessage(null, false, res.message); 
